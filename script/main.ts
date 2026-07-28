518c73193f40985e09b1c354a4be2f117bf554cc
mochinekoo <mochinekoo0113@gmail.com>
2026-07-29 01:53:01 +0900

    [add] 仮で地震情報を表示できるようになりました

---
+++ script/main.ts
@@ -0,0 +1,17 @@
+﻿function load() {
+    fetch("https://api.p2pquake.net/v2/history?codes=551")
+        .then(response => response.json())
+        .then(data => {                 
+            let earthquake = data[1].earthquake;
+            let hypocenter = earthquake.hypocenter;
+                        
+            document.getElementById("nameText").innerHTML = hypocenter.name;
+            document.getElementById("depthText").innerHTML = hypocenter.depth;
+            document.getElementById("latitudeText").innerHTML = hypocenter.latitude;
+            document.getElementById("longitudeText").innerHTML = hypocenter.longitude
+            document.getElementById("magnitudeText").innerHTML = hypocenter.magnitude;
+    });
+}
+
+load();
+
